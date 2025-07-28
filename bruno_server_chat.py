af403418795ebb6048f2a802004a16b675dfab7f
tegcic <teessideeg@gmail.com>
2025-07-28 14:21:22 +0100

    Update bruno_server_chat.py

--- bruno_server_chat.py
+++ bruno_server_chat.py
@@ -11,7 +11,7 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=api_key)
-
+app.secret_key = os.environ.get("SECRET_KEY", "dev-placeholder")
 app = Flask(__name__)
 CORS(app)
 
